6442154035760f62830b1acec705896dc238ec6b
Mickiyas-B <Mickiyasbirhanie@gmail.com>
2024-12-19 23:03:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let buttonColors = ["red", "blue", "green", "yellow"];
+
+let gamePattern =[];
+
+
+
+let randomChosenColor = buttonColors;
+
+function nextSequence(){
+    let randomNumber = Math.random()*3
+    console.log(randomNumber);
+}
